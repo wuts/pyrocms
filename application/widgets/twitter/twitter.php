4f1ba48fbad673c9f50e574d6c522d39705ff0f1
YorickPeterse <info@yorickpeterse.com>
2009-08-23 16:56:04 +0200

    Twitter widget finished :D

--- application/widgets/twitter/twitter.php
+++ application/widgets/twitter/twitter.php
@@ -16,7 +16,16 @@ class Twitter extends Widgets {
 	{
 		// Load the Twitter model
 		$this->CI->load->module_model('twitter','twitter_m');
-		echo $this->CI->twitter_m->__call('friends_timeline','');
+		
+		// Set some variables
+		$display 				= $this->get_data('twitter','display'); 
+		$limit 					= $this->get_data('twitter','limit');		
+		$data['title']			= $this->get_data('twitter','title');
+		$data['show_image'] 	= $this->get_data('twitter','show_image');
+		$data['tweets'] 		= $this->CI->twitter_m->$display($limit);
+		
+		// Load the view file 
+		$this->display('twitter','twitter',$data);
 	}
 	
 	
---
+++ application/widgets/twitter/views/twitter.php
@@ -0,0 +1,12 @@
+<h2><?php echo $title; ?></h2>
+<ul>
+	<?php foreach($tweets as $tweet): ?>
+	<li>
+		<?php if($show_image == 'true'): ?>
+		<img src="<?php echo $tweet->user->profile_image_url; ?>" alt="Profile Image" />
+		<?php endif; ?>
+		<p><strong><?php echo $tweet->user->name; ?></strong></p>
+		<p><?php echo $tweet->text; ?></p>
+	</li>
+	<?php endforeach; ?>
+</ul>
